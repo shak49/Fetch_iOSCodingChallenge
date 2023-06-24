56b53249d79d5b353f903dddf93c0cabbeebebd5
shak49 <74379616+shak49@users.noreply.github.com>
2023-06-24 11:03:37 -0500

    xcode project initiated

---
+++ Fetch_iOSCodingChallenge/Shared/Fetch_iOSCodingChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_iOSCodingChallengeApp.swift
+//  Shared
+//
+//  Created by Shak Feizi on 6/24/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_iOSCodingChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
